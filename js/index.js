f92d17e39afde698193ad97ba31100f298a8f632
Gianmario Iamoni <gianmario.iamoni@gmail.com>
2024-07-03 17:34:55 +0200

    navigation sidebar on the left

--- js/index.js
+++ js/index.js
@@ -1,12 +1,41 @@
+// const navToggle = document.querySelector('.nav-toggle');
+// const navLinks = document.querySelectorAll('.nav__link');
+
+// // navToggle.addEventListener('click', () => {
+// //     document.body.classList.toggle('nav-open');
+// // });
+
+// // JavaScript per gestire il toggle della barra laterale
+// document.querySelector('.hamburger').addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// });
+
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         document.body.classList.remove('nav-open');
+//     });
+// })
+
+// // JavaScript for toggle navigation
+// const navToggle = document.querySelector('.nav-toggle');
+// const nav = document.querySelector('.nav');
+
+// navToggle.addEventListener('click', () => {
+//     document.body.classList.toggle('nav-open');
+// });
+
+// JavaScript per gestire il toggle della barra di navigazione
 const navToggle = document.querySelector('.nav-toggle');
+// const nav = document.querySelector('.nav');
 const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
+// Chiude la barra laterale quando si clicca su una voce di navigazione
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     });
-})
+});
